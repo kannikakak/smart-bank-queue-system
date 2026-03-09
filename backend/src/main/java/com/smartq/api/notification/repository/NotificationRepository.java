902ae2a4e7dc364f88073de6285e53a0ecd55969
kannikakak <kakkannika896@gmail.com>
2026-03-10 00:09:20 +0700

    feat(notification): add customer notification query

--- backend/src/main/java/com/smartq/api/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/smartq/api/notification/repository/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.smartq.api.notification.repository;
 
 import com.smartq.api.notification.domain.Notification;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByAppointment_Customer_EmailIgnoreCaseOrderByCreatedAtDesc(String customerEmail);
 }
